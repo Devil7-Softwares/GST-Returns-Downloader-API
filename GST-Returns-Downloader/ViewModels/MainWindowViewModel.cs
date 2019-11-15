fc0b6111b51f10677e359c98cacf1c2d930f0dd3
Devil7DK <dineshthangavel47@gmail.com>
2019-11-15 23:17:14 +0530

    Initial Commit

---
+++ GST-Returns-Downloader/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GST_Returns_Downloader.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
