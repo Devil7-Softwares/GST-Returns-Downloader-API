80b694261b7320e38381f9ef2dced043558fa953
Devil7DK <dineshthangavel47@gmail.com>
2019-11-16 00:54:31 +0530

    Design initial UI & Implement basic view model

--- GST-Returns-Downloader/ViewModels/MainWindowViewModel.cs
+++ GST-Returns-Downloader/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Avalonia.Media.Imaging;
+using ReactiveUI;
 
-namespace Devil7.Automation.GSTR.Downloader.ViewModels
-{
-    public class MainWindowViewModel : ViewModelBase
-    {
-        public string Greeting => "Hello World!";
+namespace Devil7.Automation.GSTR.Downloader.ViewModels {
+    public class MainWindowViewModel : ViewModelBase {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Bitmap CaptchaImage { get; set; }
+        public string Captcha { get; set; }
+
+        public string RegisteredName { get; }
+        public string RegisteredGSTIN { get; }
     }
 }
