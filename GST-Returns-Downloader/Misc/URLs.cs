779dd48fbf2e86113d303becf61f53c17f73d539
Devil7DK <dineshthangavel47@gmail.com>
2019-11-16 12:40:12 +0530

    Add separate class for keeping URLs

---
+++ GST-Returns-Downloader/Misc/URLs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Devil7.Automation.GSTR.Downloader.Misc {
+    class URLs {
+        #region BaseURLs
+        public const string ServicesURL = "https://services.gst.gov.in/";
+        public const string ReturnsURL = "https://return.gst.gov.in/";
+        #endregion
+
+        #region Nodes
+        public const string Login = "/services/login";
+        #endregion
+    }
+}
