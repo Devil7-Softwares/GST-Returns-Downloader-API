668739a703a93770e9ea3927266ad508649dfb5d
Devil7DK <dineshthangavel47@gmail.com>
2019-11-16 12:40:38 +0530

    Implement initializing GST API

--- GST-Returns-Downloader/ViewModels/MainWindowViewModel.cs
+++ GST-Returns-Downloader/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive;
 using System.Text;
+using System.Threading.Tasks;
 using Avalonia.Media.Imaging;
+using Devil7.Automation.GSTR.Downloader.Misc;
 using ReactiveUI;
+using RestSharp;
 
 namespace Devil7.Automation.GSTR.Downloader.ViewModels {
     public class MainWindowViewModel : ViewModelBase {
+        #region Consturctor
+        public MainWindowViewModel () {
+            this.Random = new Random ();
+
+            this.InitializeAPI = ReactiveCommand.CreateFromTask (initializeAPI);
+        }
+        #endregion
+
+        #region Variables
+        private Random Random;
+        private RestClient Client;
+        #endregion
+
+        #region Properties
         public string Username { get; set; }
         public string Password { get; set; }
         public Bitmap CaptchaImage { get; set; }
@@ -16,5 +34,35 @@ namespace Devil7.Automation.GSTR.Downloader.ViewModels {
 
         public bool IsBusy { get; set; }
         public string Status { get; set; }
+        #endregion
+
+        #region Commands
+        public ReactiveCommand<Unit, Unit> InitializeAPI { get; }
+        private Task initializeAPI () {
+            return Task.Run (() => {
+                this.IsBusy = true;
+                this.Status = "Initializing API...";
+
+                try {
+                    this.Random = new Random ();
+                    this.Client = new RestClient (URLs.ServicesURL);
+                    this.Client.CookieContainer = new System.Net.CookieContainer ();
+                    this.Client.UserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36";
+
+                    RestRequest InitialRequest = new RestRequest (URLs.Login, Method.GET);
+                    RestResponse InitialResponse = (RestResponse) Client.Execute (InitialRequest);
+                    if (InitialResponse.IsSuccessful) {
+                        Console.WriteLine ("Initialization successful!");
+                    } else {
+                        Console.WriteLine (InitialResponse.ErrorMessage);
+                    }
+                } catch (Exception ex) {
+                    Console.WriteLine ("Error on initializing API: " + ex.Message);
+                } finally {
+                    this.IsBusy = false;
+                }
+            });
+        }
+        #endregion
     }
 }
--- GST-Returns-Downloader/Views/MainWindow.xaml.cs
+++ GST-Returns-Downloader/Views/MainWindow.xaml.cs
@@ -1,19 +1,30 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
-namespace Devil7.Automation.GSTR.Downloader.Views
-{
-    public class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
+namespace Devil7.Automation.GSTR.Downloader.Views {
+    public class MainWindow : Window {
+        public MainWindow () {
+            InitializeComponent ();
         }
 
-        private void InitializeComponent()
-        {
-            AvaloniaXamlLoader.Load(this);
+        private void InitializeComponent () {
+            AvaloniaXamlLoader.Load (this);
         }
+
+        #region Properties
+        private ViewModels.MainWindowViewModel ViewModel {
+            get {
+                return ((ViewModels.MainWindowViewModel) this.DataContext);
+            }
+        }
+        #endregion
+
+        #region Events
+        private void Window_Opened (object sender, EventArgs e) {
+            this.ViewModel.InitializeAPI.Execute ().Subscribe ();
+        }
+        #endregion
     }
 }
