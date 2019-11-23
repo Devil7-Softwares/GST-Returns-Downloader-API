3d8d5455b73b959a267e3ac1dc1efd0e4b5eb5cd
Devil7DK <dineshthangavel47@gmail.com>
2019-11-23 17:43:53 +0530

    Add RoleStatus Model Object

---
+++ GST-Returns-Downloader/Models/RoleStatus.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+
+namespace Devil7.Automation.GSTR.Downloader.Models
+{
+    public class Return
+    {
+        public string return_ty { get; set; }
+        public string due_dt { get; set; }
+        public string status { get; set; }
+        public bool tileDisable { get; set; }
+    }
+
+    public class User
+    {
+        public IList<Return> returns { get; set; }
+    }
+
+    public class RoleData
+    {
+        public string userType { get; set; }
+        public string userPref { get; set; }
+        public IList<User> user { get; set; }
+    }
+
+    public class RoleStatus
+    {
+        public int status { get; set; }
+        public RoleData data { get; set; }
+    }
+}
