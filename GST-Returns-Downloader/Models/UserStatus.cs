726f1558a41f953932239c11541aebba89c6c96f
Devil7DK <dineshthangavel47@gmail.com>
2019-11-23 14:15:50 +0530

    Add UserStatus Model Object

---
+++ GST-Returns-Downloader/Models/UserStatus.cs
@@ -0,0 +1,15 @@
+namespace Devil7.Automation.GSTR.Downloader.Models {
+    public class UserStatus {
+        public string bnkStat { get; set; }
+        public string role { get; set; }
+        public string stcd { get; set; }
+        public string bname { get; set; }
+        public string utype { get; set; }
+        public string IS_MIGRATED { get; set; }
+        public string Llogin { get; set; }
+        public string ASBS_STATE_CD { get; set; }
+        public string gstin { get; set; }
+        public string regType { get; set; }
+        public string appStatus { get; set; }
+    }
+}
