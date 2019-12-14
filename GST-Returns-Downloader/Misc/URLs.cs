aa783fa7ba41a13b15fdeb562d27c9f2dd18ebf8
Devil7DK <dineshthangavel47@gmail.com>
2019-12-14 14:47:06 +0530

    Add URLs for ReturnStatus, GSTR1 Generate/Download

--- GST-Returns-Downloader/Misc/URLs.cs
+++ GST-Returns-Downloader/Misc/URLs.cs
@@ -17,12 +17,16 @@ namespace Devil7.Automation.GSTR.Downloader.Misc {
         public const string KeepAlive = "/{0}/auth/api/keepalive";
         public const string Months = "/returns/auth/api/dropdown";
         public const string UserStatus = "/services/api/ustatus";
+        public const string RoleStatus = "/returns/auth/api/rolestatus?rtn_prd={0}";
+        public const string Gstr1JsonGenerateForce = "/returns/auth/api/offline/download/generate?flag=1&rtn_prd={0}&rtn_typ=GSTR1";
+        public const string Gstr1JsonGenerateOrDownload = "/returns/auth/api/offline/download/generate?flag=0&rtn_prd={0}&rtn_typ=GSTR1";
         #endregion
 
         #region FullURLs
         public const string WelcomeURL = "https://services.gst.gov.in/services/auth/fowelcome";
         public const string LoginURL = "https://services.gst.gov.in/services/login";
         public const string DashboardURL = "https://return.gst.gov.in/returns/auth/dashboard";
+        public const string GstrOfflineDownloadURL = "https://return.gst.gov.in/returns/auth/gstr/offlinedownload";
         #endregion
     }
 }
