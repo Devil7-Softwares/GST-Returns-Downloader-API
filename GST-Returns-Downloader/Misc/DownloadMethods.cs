5f06a80fab85ce2c0e7554bfedd7cf84fa5baa17
Devil7DK <dineshthangavel47@gmail.com>
2019-12-28 14:17:39 +0530

    Improve Download Methods & Add Support for GSTR2A

--- GST-Returns-Downloader/Misc/DownloadMethods.cs
+++ GST-Returns-Downloader/Misc/DownloadMethods.cs
@@ -8,12 +8,13 @@ namespace Devil7.Automation.GSTR.Downloader.Misc
 {
     public class DownloadMethods
     {
-        public static CommandResult GSTR1_JSON_GENERATE(RestClient client, string monthValue)
+        #region Private Methods
+        private static CommandResult GSTR_GENERATE(RestClient client, string monthValue, FileTypes fileType, string returnName)
         {
             CommandResult result = new CommandResult(CommandResult.Results.Failed, "Unknown Error");
             try
             {
-                RestRequest request = new RestRequest(string.Format(URLs.Gstr1JsonGenerateForce, monthValue), Method.GET);
+                RestRequest request = new RestRequest(GenerateURL(true, fileType, monthValue, returnName), Method.GET);
                 request.AddCookie("Lang", "en");
                 request.AddHeader("Referer", URLs.GstrOfflineDownloadURL);
                 RestResponse response = (RestResponse)client.Execute(request);
@@ -47,23 +48,23 @@ namespace Devil7.Automation.GSTR.Downloader.Misc
                 else
                 {
 
-                    Log.Error("Request Error on GSTR1 JSON Generate!");
+                    Log.Error(string.Format("Request Error on {0} JSON Generate!", returnName));
                 }
             }
             catch (Exception ex)
             {
-                Log.Error(ex, "Error on GSTR1 JSON Generate! " + ex.Message);
+                Log.Error(ex, string.Format("Error on {0} JSON Generate! {1}", returnName, ex.Message));
             }
 
             return result;
         }
 
-        public static CommandResult GSTR1_JSON_DOWNLOAD(RestClient client, string monthValue)
+        private static CommandResult GSTR_DOWNLOAD(RestClient client, string monthValue, FileTypes fileType, string returnName)
         {
             CommandResult result = new CommandResult(CommandResult.Results.Failed, "Unknown Error");
             try
             {
-                RestRequest request = new RestRequest(string.Format(URLs.Gstr1JsonGenerateOrDownload, monthValue), Method.GET);
+                RestRequest request = new RestRequest(GenerateURL(false, fileType, monthValue, returnName), Method.GET);
                 request.AddCookie("Lang", "en");
                 request.AddHeader("Referer", URLs.GstrOfflineDownloadURL);
                 RestResponse response = (RestResponse)client.Execute(request);
@@ -82,7 +83,7 @@ namespace Devil7.Automation.GSTR.Downloader.Misc
                             else if (returnResponse.data.status == 0 && returnResponse.data.url != null && returnResponse.data.url.Count > 0)
                             {
                                 result.Result = CommandResult.Results.Success;
-                                result.Message = "GSTR1 Downlaod Request Successful: " + monthValue;
+                                result.Message = string.Format("{0} Downlaod Request Successful: {1}", returnName, monthValue);
                                 result.Data = returnResponse.data.url;
                                 Log.Information(result.Message);
                             }
@@ -106,15 +107,63 @@ namespace Devil7.Automation.GSTR.Downloader.Misc
                 }
                 else
                 {
-                    Log.Error("Request Error on GSTR1 JSON Download!");
+                    Log.Error(string.Format("Request Error on {0} JSON Download!", returnName));
                 }
             }
             catch (Exception ex)
             {
-                Log.Error(ex, "Error on GSTR1 JSON Download! " + ex.Message);
+                Log.Error(ex, string.Format("Error on {0} JSON Download! {1}", returnName, ex.Message));
             }
 
             return result;
         }
+        #endregion
+
+        #region Public Methods
+        public static CommandResult GSTR1_JSON_GENERATE(RestClient client, string monthValue)
+        {
+            return GSTR_GENERATE(client, monthValue, FileTypes.JSON, "GSTR1");
+        }
+
+        public static CommandResult GSTR1_JSON_DOWNLOAD(RestClient client, string monthValue)
+        {
+            return GSTR_DOWNLOAD(client, monthValue, FileTypes.JSON, "GSTR1");
+        }
+
+        public static CommandResult GSTR2A_JSON_GENERATE(RestClient client, string monthValue)
+        {
+            return GSTR_GENERATE(client, monthValue, FileTypes.JSON, "GSTR2A");
+        }
+
+        public static CommandResult GSTR2A_JSON_DOWNLOAD(RestClient client, string monthValue)
+        {
+            return GSTR_DOWNLOAD(client, monthValue, FileTypes.JSON, "GSTR2A");
+        }
+
+        public static CommandResult GSTR2A_EXCEL_GENERATE(RestClient client, string monthValue)
+        {
+            return GSTR_GENERATE(client, monthValue, FileTypes.EXCEL, "GSTR2A");
+        }
+
+        public static CommandResult GSTR2A_EXCEL_DOWNLOAD(RestClient client, string monthValue)
+        {
+            return GSTR_DOWNLOAD(client, monthValue, FileTypes.EXCEL, "GSTR2A");
+        } 
+        #endregion
+
+        #region Enums
+        private enum FileTypes
+        {
+            JSON,
+            EXCEL
+        }
+        #endregion
+
+        #region Private Helper Methods
+        private static string GenerateURL(bool forceGenerate, FileTypes fileType, string returnPeriod, string returnName)
+        {
+            return string.Format(URLs.GstrReturnGenerateOrDownload, fileType == FileTypes.EXCEL ? "file_type=EX&" : "", forceGenerate ? 1 : 0, returnPeriod, returnName);
+        }
+        #endregion
     }
 }
--- GST-Returns-Downloader/Misc/URLs.cs
+++ GST-Returns-Downloader/Misc/URLs.cs
@@ -20,8 +20,7 @@ namespace Devil7.Automation.GSTR.Downloader.Misc
         public const string Months = "/returns/auth/api/dropdown";
         public const string UserStatus = "/services/api/ustatus";
         public const string RoleStatus = "/returns/auth/api/rolestatus?rtn_prd={0}";
-        public const string Gstr1JsonGenerateForce = "/returns/auth/api/offline/download/generate?flag=1&rtn_prd={0}&rtn_typ=GSTR1";
-        public const string Gstr1JsonGenerateOrDownload = "/returns/auth/api/offline/download/generate?flag=0&rtn_prd={0}&rtn_typ=GSTR1";
+        public const string GstrReturnGenerateOrDownload = "/returns/auth/api/offline/download/generate?{0}flag={1}&rtn_prd={2}&rtn_typ={3}";
         #endregion
 
         #region FullURLs
